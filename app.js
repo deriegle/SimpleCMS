314161aaa9f3f59095699a293a9cc4e6b58589c4
Joseph B. Cruz <josephbriancruz1@gmail.com>
2017-04-21 16:24:09 -0400

    Added Create and Edit post functionality
    User can now create new posts and edit posts.
    
    Added Dependencies:
    
    body-parser
    method-override

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express 	= require("express");
 	seedDB		= require("./seeds.js");
 	PORT		= 3001,
 	TITLE		= "Simple CMS",
-	app 		= express();
+	app 		= express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/SimpleCMS");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 const site = {
 	title: TITLE,
